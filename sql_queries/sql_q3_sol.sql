d9609a536b7a39940e331e62917d397fd500758c
caseyfrost <caseyfrost13@gmail.com>
2022-02-13 19:11:21 -0800

    question 4 and 5

---
+++ sql_queries/sql_q3_sol.sql
@@ -0,0 +1,3 @@
+SELECT *
+FROM euro_cup_2016.player_in_out
+INNER JOIN euro_cup_2016.match_mast;
---
+++ sql_queries/sql_q4_sol.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(p.match_no), m.play_stage
+FROM euro_cup_2016.player_in_out as p
+INNER JOIN euro_cup_2016.match_mast as m
+GROUP BY m.play_stage;
