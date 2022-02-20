6086c73ffb3c620ac747f8ba90a75189f0c8c2d2
caseyfrost <caseyfrost13@gmail.com>
2022-02-20 10:21:12 -0800

    replaced CTE with self join for slight performance improvement

--- sql_queries/sql_q6_sol.sql
+++ sql_queries/sql_q6_sol.sql
@@ -1,3 +1,30 @@
+-- One CTE of a self join
+WITH difs AS (
+SELECT
+	m1.match_no,
+	m1.goal_score - m2.goal_score AS dif
+FROM
+	euro_cup_2016.match_details AS m1
+INNER JOIN
+	euro_cup_2016.match_details AS m2
+		ON
+			m1.match_no = m2.match_no
+		AND
+			m1.decided_by = 'N'
+		AND
+			m1.win_lose != m2.win_lose
+		AND
+			m1.win_lose = 'W')
+            
+SELECT
+	COUNT(match_no) AS count_matches_won_by_one_point
+FROM
+	difs
+WHERE
+	dif = 1;
+
+
+-- Multiple CTE without self join
 WITH wins AS (
 SELECT
 	match_no,
